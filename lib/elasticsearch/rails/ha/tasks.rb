c9ed07dc0c6f0f64c0653af7662b6527686c07bb
Peter Karman <peter.karman@gsa.gov>
2016-01-07 21:06:53 -0600

    change rake task namespace to `rake elasticsearch:ha` and add `promote` task

--- lib/elasticsearch/rails/ha/tasks.rb
+++ lib/elasticsearch/rails/ha/tasks.rb
@@ -3,14 +3,14 @@
 require 'elasticsearch/rails/ha'
 
 namespace :elasticsearch
-  namespace :import
+  namespace :ha
 
     desc "import records in parallel"
     task :parallel do
       nprocs     = ENV['NPROCS'] || 1
       batch_size = ENV['BATCH']  || 100
       max        = ENV['MAX']    || nil
-      klass      = ENV['CLASS']
+      klass      = ENV['CLASS'] or fail "CLASS required"
 
       indexer = Elasticsearch::Rails::HA::ParallelIndexer.new(
         klass: klass, 
@@ -26,11 +26,11 @@ namespace :elasticsearch
     end
 
     desc "stage an index"
-    task :stage
+    task :stage do
       nprocs     = ENV['NPROCS'] || 1
       batch_size = ENV['BATCH']  || 100 
       max        = ENV['MAX']    || nil 
-      klass      = ENV['CLASS']
+      klass      = ENV['CLASS'] or fail "CLASS required"
 
       stager = Elasticsearch::Rails::HA::IndexStager.new(klass)
       indexer = Elasticsearch::Rails::HA::ParallelIndexer.new(
@@ -46,6 +46,14 @@ namespace :elasticsearch
       stager.alias_index
       stager.clean_up_old_indices
       puts "[#{Time.now.utc.iso8601}] #{klass} index staged as #{stager.stage_index_name}"
+    end
+
+    desc "promote staged index to live"
+    task :promote do
+      klass = ENV['CLASS'] or fail "CLASS required"
+      stager = Elasticsearch::Rails::HA::IndexStager.new(klass)
+      stager.promote
+      puts "[#{Time.now.utc.iso8601}] #{klass} promoted #{stage_index_name} to #{klass.index_name}"
     end
 
   end
