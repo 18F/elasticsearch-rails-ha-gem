438b087d9ce0ac4807e3c1975c6933894a8a273c
Peter Karman <peter.karman@gsa.gov>
2016-02-05 15:15:28 -0600

    fix variable assignment bug where promote() was failing to detect existing non-aliased live index

--- lib/elasticsearch/rails/ha/index_stager.rb
+++ lib/elasticsearch/rails/ha/index_stager.rb
@@ -31,23 +31,23 @@ module Elasticsearch
         end
 
         def promote(live_index_name=klass.index_name)
-          @live_index_name = live_index_name
+          @live_index_name = live_index_name || klass.index_name
 
           # the renaming actions (performed atomically by ES)
           rename_actions = [
             { remove: { index: stage_aliased_to, alias: stage_index_name } },
-            {    add: { index: stage_index_name, alias: live_index_name } }
+            {    add: { index: stage_index_name, alias: @live_index_name } }
           ]
 
           # zap any existing index known as index_name,
           # but do it conditionally since it is reasonable that it does not exist.
           to_delete = []
-          existing_live_index = es_client.indices.get_aliases(index: live_index_name)
+          existing_live_index = es_client.indices.get_aliases(index: @live_index_name)
           existing_live_index.each do |k,v|
 
             # if the index is merely aliased, remove its alias as part of the aliasing transaction.
-            if k != klass.index_name
-              rename_actions.unshift({ remove: { index: k, alias: live_index_name } })
+            if k != @live_index_name
+              rename_actions.unshift({ remove: { index: k, alias: @live_index_name } })
 
               # mark it for deletion when we've successfully updated aliases
               to_delete.push k
@@ -56,7 +56,7 @@ module Elasticsearch
               # this is a real, unaliased index with this name, so it must be deleted.
               # (This usually happens the first time we implement the aliasing scheme against
               # an existing installation.)
-              es_client.indices.delete index: live_index_name rescue false
+              es_client.indices.delete index: @live_index_name rescue false
             end
           end
 
@@ -82,18 +82,23 @@ module Elasticsearch
         def stage_aliased_to
           # find the newest tmp index to which staged is aliased.
           # we need this because we want to re-alias it.
+          aliased_to = find_newest_alias_for(stage_index_name)
+        end
+
+        def find_newest_alias_for(the_index_name)
           aliased_to = nil
-          stage_aliases = es_client.indices.get_aliases(index: stage_index_name)
-          stage_aliases.each do |k,v|
+          aliases = es_client.indices.get_aliases(index: the_index_name)
+          aliases.each do |k,v|
+            next unless k.match(tmp_index_pattern)
             aliased_to ||= k
-            stage_tstamp = aliased_to.match(tmp_index_pattern)[1]
+            alias_tstamp = aliased_to.match(tmp_index_pattern)[1]
             k_tstamp = k.match(tmp_index_pattern)[1]
-            if Time.parse(stage_tstamp) < Time.parse(k_tstamp)
+            if Time.parse(alias_tstamp) < Time.parse(k_tstamp)
               aliased_to = k
             end
           end
           if !aliased_to
-            raise "Cannot identify index aliased to by '#{stage_index_name}'"
+            raise "Cannot identify index aliased to by '#{the_index_name}'"
           end
           aliased_to
         end
